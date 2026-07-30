461484799763a7036d2eed07007632b508027b65
yogita-mehta <yogitamehta3488@gmail.com>
2026-07-31 01:57:12 +0530

    fix: auto-format files to resolve gci and wsl lint errors

--- pkg/resolver/cel.go
+++ pkg/resolver/cel.go
@@ -28,12 +28,11 @@ import (
 	"github.com/google/cel-go/common/types/ref"
 	"github.com/google/cel-go/interpreter"
 	"github.com/prometheus/client_golang/prometheus"
-
 	"k8s.io/apimachinery/pkg/api/resource"
 	"k8s.io/klog/v2"
 
-	"github.com/kubernetes-sigs/resource-state-metrics/pkg/metricutil"
-	"github.com/kubernetes-sigs/resource-state-metrics/pkg/options"
+	"sigs.k8s.io/resource-state-metrics/pkg/metricutil"
+	"sigs.k8s.io/resource-state-metrics/pkg/options"
 )
 
 var (
