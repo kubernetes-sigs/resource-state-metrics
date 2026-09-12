26e1fb9963a456295f125ec0f8a92358474409a7
Aditya Raj <adityaraj10600@gmail.com>
2026-09-12 17:04:55 +0000

    test: add unit tests for validateValue function
    Signed-off-by: Aditya Raj <adityaraj10600@gmail.com>

--- internal/metric_test.go
+++ internal/metric_test.go
@@ -14,3 +14,61 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 package internal
+
+import (
+	"math"
+	"testing"
+)
+
+func TestValidateValue(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		floatVal float64
+		kind     MetricKind
+		wantErr  bool
+	}{
+		{
+			name:     "gauge allows NaN",
+			floatVal: math.NaN(),
+			kind:     MetricKindGauge,
+			wantErr:  false,
+		},
+		{
+			name:     "gauge allows negative value",
+			floatVal: -1,
+			kind:     MetricKindGauge,
+			wantErr:  false,
+		},
+		{
+			name:     "counter allows non-negative value",
+			floatVal: 1,
+			kind:     MetricKindCounter,
+			wantErr:  false,
+		},
+		{
+			name:     "counter rejects NaN",
+			floatVal: math.NaN(),
+			kind:     MetricKindCounter,
+			wantErr:  true,
+		},
+		{
+			name:     "counter rejects negative value",
+			floatVal: -1,
+			kind:     MetricKindCounter,
+			wantErr:  true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			err := validateValue(tt.floatVal, tt.kind)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("validateValue(%v, %v) error = %v, wantErr %v", tt.floatVal, tt.kind, err, tt.wantErr)
+			}
+		})
+	}
+}
